774d44d098cc6848ed99adf39e260136a37788e5
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2020-09-14 18:51:50 +0800

    Recreate modifications in new workflow

--- __nexi-client__.js
+++ __nexi-client__.js
@@ -1,3 +1,119 @@
 const client = () => {
   console.log("Welcome to NeXi client");
+  customInit();
+};
+
+const customInit = () => {
+  customOnLeaveCallback();
+  customTransition();
+  customMatchFoundAnimation();
+};
+
+// Functionality modifications
+
+const customOnLeaveCallback = () => {
+  Player.prototype.onLeave = function () {
+    this.app.mouse.disablePointerLock(),
+      Utils.isMobile()
+        ? (window.location.href =
+            "https://beta-meta-42746.venge.io/?isMobile=yes&v=" + Math.random())
+        : (window.location.href = "index.html");
+  };
+};
+
+// UI Modifications
+
+const customTransition = () => {
+  Overlay.prototype.onTransition = function (t) {
+    t
+      ? ((this.leftCinema.element.color = t),
+        (this.rightCinema.element.color = t))
+      : ((this.leftCinema.element.color = pc.colors.black),
+        (this.rightCinema.element.color = pc.colors.black)),
+      (this.leftCinema.enabled = !0),
+      (this.rightCinema.enabled = !0),
+      (this.entity.sound.slots.Whoosh.pitch = 1.1),
+      this.entity.sound.play("Whoosh"),
+      this.leftCinema.setLocalEulerAngles(0, 0, 0),
+      this.leftCinema.setLocalScale(0.1, 0, 0),
+      this.leftCinema
+        .tween(this.leftCinema.getLocalScale())
+        .to({ x: 1.4, y: 1, z: 1 }, 0.35, pc.QuarticInOut)
+        .start(),
+      this.rightCinema.setLocalEulerAngles(0, 0, 0),
+      this.rightCinema.setLocalScale(0.1, 0, 0),
+      this.rightCinema
+        .tween(this.rightCinema.getLocalScale())
+        .to({ x: 1.4, y: 1, z: 1 }, 0.35, pc.QuarticInOut)
+        .start(),
+      setTimeout(
+        function (t) {
+          t.leftCinema.setLocalEulerAngles(0, 0, 0),
+          t.leftCinema
+            .tween(t.leftCinema.getLocalScale())
+            .to({ x: 0.1, y: 1, z: 1 }, 0.35, pc.QuarticInOut)
+            .start(),
+          t.rightCinema.setLocalEulerAngles(0, 0, 0),
+          t.rightCinema
+            .tween(t.rightCinema.getLocalScale())
+            .to({ x: 0.1, y: 1, z: 1 }, 0.35, pc.QuarticInOut)
+            .start(),
+          (t.entity.sound.slots.Whoosh.pitch = 1),
+          t.entity.sound.play("Whoosh");
+        },
+        400,
+        this
+      );
+  };
+};
+
+const customMatchFoundAnimation = () => {
+  Menu.prototype.onMatchFound = function () {
+    (this.isMatchFound = !0),
+      (this.app.scene.layers.getLayerByName("Lightroom").enabled = !1),
+      (this.app.scene.layers.getLayerByName("Lightroom-Top").enabled = !1),
+      clearTimeout(this.bannerTimeout),
+      this.app.fire("Ads:BannerDestroy", "venge-io_728x90", "728x90"),
+      this.app.fire("DOM:Clear", !0),
+      this.app.off("Player:Character"),
+      this.app.fire("Popup:Close", !0),
+      (this.matchFoundBackground.enabled = !0),
+      this.matchFoundBackground
+        .tween(this.matchFoundBackground.element)
+        .to({ opacity: 1 }, 1, pc.QuarticOut)
+        .start(),
+      (this.matchFoundRectangle.element.opacity = 1),
+      this.matchFoundRectangle.setLocalScale(20, 1, 1),
+      this.matchFoundCenter.setLocalScale(3, 3, 3),
+      this.matchFoundRectangle
+        .tween(this.matchFoundRectangle.getLocalScale())
+        .to({ x: 1, y: 1, z: 1 }, 0.5, pc.QuarticOut)
+        .start(),
+      this.matchFoundRectangle
+        .tween(this.matchFoundRectangle.element)
+        .to({ opacity: 0.1 }, 0.5, pc.QuarticOut)
+        .start(),
+      this.matchFoundCenter
+        .tween(this.matchFoundCenter.getLocalScale())
+        .to({ x: 1.2, y: 1.2, z: 1.2 }, 2, pc.QuarticOut)
+        .start(),
+      setTimeout(
+        function (e) {
+          (e.matchFoundLoading.enabled = !0),
+            e.matchFoundRectangle
+              .tween(e.matchFoundRectangle.element)
+              .to({ opacity: 0 }, 0.5, pc.QuarticOut)
+              .start(),
+            e.matchFoundText
+              .tween(e.matchFoundText.element)
+              .to({ opacity: 0 }, 0.5, pc.QuarticOut)
+              .start(),
+            setTimeout(function () {
+              pc.app.fire("Game:Connect", !0);
+            }, 1000);
+        },
+        1500,
+        this
+      );
+  };
 };
