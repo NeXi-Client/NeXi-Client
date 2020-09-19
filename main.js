cb7d8a57f96fbac96d3d3a7d2e0001a2b264ae27
VengeUrban <71067316+VengeUrban@users.noreply.github.com>
2020-09-19 08:55:32 +0800

    Fixed Glitches, Neatened Code

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const { app, shell } = require('electron')
-const { BrowserWindow } = require('electron')
-const { globalShortcut } = require('electron')
-const { dialog } = require('electron')
+const {
+    app, shell
+} = require('electron')
+const {
+    BrowserWindow
+} = require('electron')
+const {
+    globalShortcut
+} = require('electron')
+const {
+    dialog
+} = require('electron')
 const prompt = require('electron-prompt')
 const RPC = require('discord-rpc')
-const isDev = require('electron-is-dev')
-const appVersion = app.getVersion()
+
 
 app.commandLine.appendSwitch('disable-frame-rate-limit')
 app.commandLine.appendSwitch('disable-gpu-vsync')
@@ -29,184 +36,183 @@ app.commandLine.appendSwitch('enable-pointer-lock-options');
 app.commandLine.appendSwitch('disable-accelerated-video-decode', false);
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required')
 app.commandLine.appendSwitch('enable-quic');
-app.commandLine.appendSwitch('high-dpi-support',1);
+app.commandLine.appendSwitch('high-dpi-support', 1);
 
-function init()
-{
-	DiscordRPC();
-	createWindow();
-	shortCuts();
-	Leave();
-	CheckURL();
+function init() {
+
+    DiscordRPC();
+    createWindow();
+    shortCuts();
+    CheckGame();
+    leave();
 }
 
-function DiscordRPC()
-{
-	const rpc = new RPC.Client({ transport: "ipc" });
-	rpc.on("ready", () => {
-		rpc.setActivity({
-			state: 'Playing Venge.io',
-			startTimestamp: new Date(),
-			largeImageKey: 'nexi-old',
-		})
-	});
+function DiscordRPC() {
+    const rpc = new RPC.Client({
+        transport: "ipc"
+    });
+    rpc.on("ready", () => {
+        rpc.setActivity({
+            state: 'Playing Venge.io',
+            startTimestamp: new Date(),
+            largeImageKey: 'nexi-old',
+        })
+    });
 
-	console.log("Rich presence is now active")
+    console.log("Rich presence is now active")
 
-	rpc.login({ clientId: "750116161890287657" })
-}
-
-//Erstellt das Fenster für Venge.io
-function createWindow()
-{
-	win = new BrowserWindow({ 
-		width: 1920, height: 1080, icon: "files/game.png" 
-		})
-	win.loadFile('index.html')
-	win.setFullScreen(true)
-	win.removeMenu(true)
-	win.setTitle('NeXi-Client')
-	win.on('page-title-updated', function(e) { e.preventDefault() })
-	console.log('Window has been created')
-}
-
-function CheckURL(){
-	String.prototype.CheckVenge = function() {
-		var SITE_REGEX = /^(https?:\/\/)?(www\.)?(.+\.|)(venge\.io|127\.0\.0\.1:8080)(|\/|.+)$/;
-		return SITE_REGEX.test(this, '')
-	}
-	let nav = (e, url) => {
-		e.preventDefault();
-		if (url.CheckVenge()) {
-			win.loadURL(url);
-		} 
-		else shell.openExternal(url);
-	};
-
-	win.webContents.on('new-window', nav);
-	win.webContents.on('will-navigate', nav);
-}
-
-function Leave()
-{
-	win.webContents.on('will-prevent-unload', (event) => {
-        const choice = dialog.showMessageBoxSync(win, {
-            type: 'question',
-            buttons: ['Leave', 'Stay'],
-            title: 'Are you sure you want to leave?',
-            message: 'You are in the middle of the game currently.',
-            defaultId: 0,
-            cancelId: 1
-        });
-        const leave = (choice === 0)
-        if (leave) {
-            event.preventDefault();
-
-        };
+    rpc.login({
+        clientId: "750116161890287657"
     })
 }
 
-function shortCuts()
-{
-	globalShortcut.register('F1', () => { 
-		win.loadFile('index.html'), console.log('Loading assets') 
-	})
-	globalShortcut.register('F2', () => { 
-		console.log('Linkbox has been opened'), LinkBox() 
-	})
-	globalShortcut.register('Alt+F4', () => { 
-		win.webContents.executeJavaScript(onbeforeunload = null),
-		console.log('Quit has been used'),
-		app.exit(0)  
-	}) 
-	globalShortcut.register('Ctrl+F5',() =>{
-		win.webContents.reloadIgnoringCache();
-	}) 
-	globalShortcut.register('F9', () => { 
-		win.webContents.openDevTools(), 
-		console.log('DevTools opened') 
-	})
-    	globalShortcut.register('F11', () => { 
-		win.setSimpleFullScreen(!win.isSimpleFullScreen()) 
-	})
-	globalShortcut.register('ESCAPE', () => { 
-	win.webContents.executeJavaScript(`
-                  document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
-                  document.exitPointerLock();
-        `);
-	})
-	console.log('Shortcuts has been registered')
+//Create window
+function createWindow() {
+    win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        icon: "files/game.png"
+    })
+    win.loadFile('index.html')
+    win.setFullScreen(true)
+    win.removeMenu(true)
+    win.setTitle('NeXi-Client')
+    win.on('page-title-updated', function(e) {
+        e.preventDefault()
+    })
+    console.log('Window has been created')
+
 }
 
-//Funktion damit die Linkbox erstellt wird
-function LinkBox()
-{	
-	const choice = dialog.showMessageBoxSync(win, {
-		type: 'question',
-		buttons: ['Play', 'Spectate'],
-		title: 'Join',
-		message: 'Would you like to spectate or play?',
-		defaultId: 0,
-		cancelId: 2
-	});
-	const link = (choice === 0)
-	if (link) {
-		prompt({
-			title: 'Play',
-			label: 'Please enter your Invite link here',
-			value: 'https://venge.io/#00000',
-			inputAttrs: {
-				type: 'url'
-			},
-			type: 'input'
-			})
-			.then((r) => {
-				let inputUrl = r;
-				let arr1 = inputUrl.split('#');
-				let inviteCode = arr1[arr1.length - 1];
-				
-				let currentUrl = win.webContents.getURL();
-				let arr2= currentUrl.split('/');
-				
-				arr2[arr2.length - 1] = `index.html#${inviteCode}`;
-				
-				let newUrl = arr2.join('/');
-				
-				console.log(newUrl)
-				win.loadURL(newUrl);
-			});
-		}
-	else {
-		if (choice !== 2) {
-			prompt({
-				title: 'Spectate',
-				label: 'Please enter your Invite link here',
-				value: 'https://venge.io/#00000',
-				inputAttrs: {
-					type: 'url'
-				},
-				type: 'input'
-				})
-				.then((r) => {
-					let inputUrl = r;
-					let arr1 = inputUrl.split('#');
-					let inviteCode = arr1[arr1.length - 1];
-					
-					let currentUrl = win.webContents.getURL();
-					let arr2= currentUrl.split('/');
-					
-					arr2[arr2.length - 1] = `index.html#Spectate:${inviteCode}`;
-					
-					let newUrl = arr2.join('/');
-					
-					console.log(newUrl)
-					win.loadURL(newUrl);
-				});
-			}
-		}
-	};
+function CheckGame() {
+    String.prototype.CheckForTwitch = function() {
+        var SITE_REGEX = /https:\/\/www.twitch.tv/;
+        return SITE_REGEX.test(this, '');
+    }
+    String.prototype.CheckForYT = function() {
+        var YT_REGEX = /https:\/\/www.youtube.com/;
+        return YT_REGEX.test(this, '');
+    }
+    let nav = (e, url) => {
+        e.preventDefault();
+        if (!url.CheckForYT() && !url.CheckForTwitch()) {
+            win.loadURL(url);
+        } else {
+            shell.openExternal(url);
+        }
+    };
+
+    win.webContents.on('new-window', nav);
+    win.webContents.on('will-navigate', nav);
+
+}
+
+function leave() {
+    win.webContents.on('will-prevent-unload', (event) => {
+        win.webContents.executeJavaScript(`
+		onbeforeunload = null;
+		`)
+        event.preventDefault();
+    })
+}
+
+//Asks for link and inputs it.
+function LinkBox() {
+    const choice = dialog.showMessageBoxSync(win, {
+        type: 'question',
+        buttons: ['Play', 'Spectate'],
+        title: 'Join',
+        message: 'Would you like to spectate or play?',
+        defaultId: 0,
+        cancelId: 2
+    });
+    const link = (choice === 0)
+    if (link) {
+        prompt({
+                title: 'Play',
+                label: 'Please enter your Invite link here',
+                value: 'https://venge.io/#00000',
+                inputAttrs: {
+                    type: 'url'
+                },
+                type: 'input'
+            })
+            .then((r) => {
+                let inputUrl = r;
+                let arr1 = inputUrl.split('#');
+                let inviteCode = arr1[arr1.length - 1];
+
+                let currentUrl = win.webContents.getURL();
+                let arr2 = currentUrl.split('/');
+
+                arr2[arr2.length - 1] = `index.html#${inviteCode}`;
+
+                let newUrl = arr2.join('/');
+
+                console.log(newUrl)
+                win.loadURL(newUrl);
+            });
+    } else {
+        if (choice !== 2) {
+            prompt({
+                    title: 'Spectate',
+                    label: 'Please enter your Invite link here',
+                    value: 'https://venge.io/#00000',
+                    inputAttrs: {
+                        type: 'url'
+                    },
+                    type: 'input'
+                })
+                .then((r) => {
+                    let inputUrl = r;
+                    let arr1 = inputUrl.split('#');
+                    let inviteCode = arr1[arr1.length - 1];
+
+                    let currentUrl = win.webContents.getURL();
+                    let arr2 = currentUrl.split('/');
+
+                    arr2[arr2.length - 1] = `index.html#Spectate:${inviteCode}`;
+
+                    let newUrl = arr2.join('/');
+
+                    console.log(newUrl)
+                    win.loadURL(newUrl);
+                });
+        }
+    }
+};
 
 
-	
 
+//Shortcuts
+function shortCuts() {
+    globalShortcut.register('F1', () => {
+        win.loadFile('index.html'), console.log('Loading assets')
+    })
+    globalShortcut.register('F2', () => {
+        console.log('Linkbox has been opened'), LinkBox()
+    })
+    globalShortcut.register('Alt+F4', () => {
+        win.webContents.executeJavaScript(onbeforeunload = null),
+            console.log('Quit has been used'),
+            app.exit(0)
+    })
+    globalShortcut.register('Ctrl+F5', () => {
+        win.webContents.reloadIgnoringCache();
+    })
+    globalShortcut.register('F9', () => {
+        win.webContents.openDevTools(),
+            console.log('DevTools opened')
+    })
+    globalShortcut.register('F11', () => {
+        win.setSimpleFullScreen(!win.isSimpleFullScreen())
+    })
+    globalShortcut.register('ESCAPE', () => {
+        win.webContents.executeJavaScript(`
+                  document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+                  document.exitPointerLock();
+		`);
+    })
+    console.log('Shortcuts has been registered')
+}
 app.on('ready', init)
